d8c628e10679ccedbc13ef279adb5bd803c4a58e
S <s>
2015-01-07 23:25:44 -0800

    {"type":"TI", "runs":"0"}

--- Date.java
+++ Date.java
@@ -1,4 +1,27 @@
+/*
+ * File: Data.java
+ * ---------------------
+ * Adds the seconds interval to the given ISO 8601 datestamp, forms a new
+ * stamp, and returns it.
+ */
 
-public class Date {
+import acm.program.*;
+
+public class Date extends ConsoleProgram {
+	
+	public void run(){
+		int interval = 64204600;
+		int seconds = interval % 60;
+		int minutesTemporary = interval / 60;
+		
+		int minutes = minutesTemporary % 60;
+		int hoursTemporary = minutesTemporary / 60;
+		int hours = hoursTemporary % 24;
+		int daysTemporary = hoursTemporary / 24;
+		int days = daysTemporary % 24;
+		
+		println(days + " " + hours  + " " + minutes + " " + seconds);
+		
+	}
 
 }
