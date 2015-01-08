7688eb1791c440768c059d17757f2c5f485b8696
S <s>
2015-01-07 21:44:51 -0800

    {"type":"TI", "runs":"0"}

--- BlankClass.java
+++ BlankClass.java
@@ -10,7 +10,18 @@ import acm.program.*;
 
 public class BlankClass extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		String word = readLine("Enter the string you want reversed: ");
+		println("Here is the reversed string: " + reverseString(word));
+	}
+	
+	private String reverseString(String word) {
+		String result = "";
+		
+		for (int i = word.length() - 1; i >= 0; i--) {
+			result += word.charAt(i);
+		}
+		
+		return result;
 	}
 }
 
