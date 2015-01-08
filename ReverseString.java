a4bd68eb14099479e41b7fbcbb67d4be564e3acf
S <s>
2015-01-07 21:55:16 -0800

    {"type":"TI", "runs":"0"}

--- ReverseString.java
+++ ReverseString.java
@@ -1,9 +1,7 @@
 /*
- * File: BlankClass.java
+ * File: ReverseString.java
  * ---------------------
- * This class is a blank one that you can change at will. Remember, if you change
- * the class name, you'll need to change the filename so that it matches.
- * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
+ * Reverse the string inputed by the user
  */
 
 import acm.program.*;
