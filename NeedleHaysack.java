c1ed0320024325f12aaf95cdf4e715a4c94d479a
S <s>
2015-01-07 21:56:34 -0800

    {"type":"TI", "runs":"1"}

--- NeedleHaysack.java
+++ NeedleHaysack.java
@@ -8,11 +8,12 @@ import acm.program.*;
 public class NeedleHaysack extends ConsoleProgram {
 	
 	public void run() {
-		String needle = readLine("Enter needle: ");
+
 		String[] haystack = {"CsL7W","F5gXp","RShbj","rl4uj","rG0kb","iC7EQ","eyGik",
 		                     "MlJQu","HfkQb","bsK9x","0CrXg","QwXJL","hr7eU","4ytwM",
 		                     "zfyrH","TRsbq","VJr1c","e14WO","njqMl","o2FwM"};
-		println(getNeedleIndex(needle, haystack));
+		String needle = readLine("Enter needle: ");
+		println("The index of the needle int he haystack is: " + getNeedleIndex(needle, haystack));
 	}
 	
 	private int getNeedleIndex(String needle, String[] haystack) {
